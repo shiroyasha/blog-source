441d549caadae13a84737e4d9a064ce8938a7604
Igor Sarcevic <igisar@gmail.com>
2015-03-17 18:55:09 +0100

    Minify css and js

--- config.rb
+++ config.rb
@@ -94,11 +94,8 @@ set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_css
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :asset_hash
