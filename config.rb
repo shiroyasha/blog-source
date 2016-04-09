85abb03dd787e583eb3dcaae498c2c7aeef34591
Igor Šarčević <igor@renderedtext.com>
2016-04-09 14:22:36 +0200

    Add about page

--- config.rb
+++ config.rb
@@ -10,17 +10,17 @@ end
 page "/sitemap.xml", :layout => false
 page "/feed.xml", :layout => false
 page "/index.html", :layout => "layout"
+page "/about.html", :layout => "layout"
 
 ignore "article_layout.erb"
 
 set :markdown_engine, :redcarpet
 set :markdown, :fenced_code_blocks => true, :smartypants => true
+
 activate :syntax
 
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
 
 configure :build do
